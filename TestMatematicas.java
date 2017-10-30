535051cb204de3381fb34dcac7e4aa91b03cd89f
Alex <alejandro4703@hotmail.es>
2017-10-30 14:43:11 +0100

    modificacion de los programas dobles en matematicas

---
+++ TestMatematicas.java
@@ -0,0 +1,7 @@
+public class TestMatematicas {
+	public static void main (String [] args){
+	int numero= 15;
+	boolean resultado = Matematicas.esPar(numero);
+	System.out.println ("El numero " +numero + "¿Es par? : " + resultado);
+}
+ }
