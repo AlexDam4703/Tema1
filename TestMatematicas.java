013d1d5b15c7ab0fc272c1b17e27230e6bcd2294
Alex <alejandro4703@hotmail.es>
2017-10-31 11:09:56 +0100

    nuevos metodos para la divion por tres y por cinco

--- TestMatematicas.java
+++ TestMatematicas.java
@@ -2,6 +2,10 @@ public class TestMatematicas {
 	public static void main (String [] args){
 	int numero= 15;
 	boolean resultado = Matematicas.esPar(numero);
+	boolean resultadoTres = Matematicas.esDivisiblePorTres(numero);
+	boolean resultadoCinco = Matematicas.esDivisiblePorCinco(numero);
 	System.out.println ("El numero " +numero + "¿Es par? : " + resultado);
+	System.out.println ("El numero " +numero + "¿Es dividible entre tres? : " + resultadoTres);
+	System.out.println ("El numero " +numero + "¿Es divisible entre cienco? : " + resultadoCinco);
 }
  }
